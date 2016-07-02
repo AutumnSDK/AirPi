01b690aeb676db8a242b025d293c6bf026d17cfd
Andy Clark <andy@pencilsandpixels.com>
2016-07-02 17:25:28 +1000

    Update
    Update code from slack

--- autumn.py
+++ autumn.py
@@ -10,11 +10,12 @@ class Autumn(output.Output):
 
    def __init__(self, data):
 
-       for key, value in data.items():
+       self.name = type(self).__name__
+       self.params = {}
+       for key, value in data.items('Autumn'):
            setattr(self, key, value)
-
        self.datetime_format = "%Y-%m-%dT%H:%M:%S.000Z"
-       
+
    def _generate_body(self, point):
 
        today = datetime.datetime.now()
@@ -45,21 +46,21 @@ class Autumn(output.Output):
            'ORGANIZATION-ID': self.organization_key,
            'ORGANIZATION-API-KEY': self.api_key,
            'Content-Type': 'application/vnd.api+json'
-       }
+		   }
 
-   def _generate_path(self):
-       return '/api/external/v1/sensors/%s/sensordata/' % self.element_id
+		      def _generate_path(self):
+		          return '/api/external/v1/sensors/%s/sensordata/' % self.element_id
 
-   def outputData(self, dataPoints):
+		      def output_data(self, dataPoints, *args, **kwargs):
 
-       for i in dataPoints:
-           try:
-               response = requests.post(
-                   self._generate_path(), headers=self._generate_headers(), data=self._generate_body(i))
+		          for i in dataPoints:
+		              try:
+		                  response = requests.post(
+		                      self._generate_path(), headers=self._generate_headers(), data=self._generate_body(i))
 
-               if response.status_code != 201:
-                   return False
-               
-           except Exception:
-               return False
-       return True
+		                  if response.status_code != 201:
+		                      return False
+
+		              except Exception:
+		                  return False
+		          return True
